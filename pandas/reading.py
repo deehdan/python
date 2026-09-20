9700f6e6208c03545d84af529009ddb4f86e9a79
DEEDAN <waruidedan12@gmail.com>
2026-09-20 23:51:59 +0300

    Fix comment

--- pandas/reading.py
+++ pandas/reading.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 data = pd.read_csv("pandas/data.csv")
 
-# print(data) => only prints the first and last 5
+# print(data) => only prints the first and last 5 rows
 
 print(data.to_string())
