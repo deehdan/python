0d3cf715e0cb5cb90326583c2e4f9242fe9005ae
deehdan <waruidedan12@gmail.com>
2026-09-22 11:14:22 +0300

    done with the project

--- pandas/project/project.py
+++ pandas/project/project.py
@@ -1,5 +1,38 @@
 import pandas as pd
 
-students = pd.read_csv("project/student.py")
+students = pd.read_csv("pandas/project/student.csv")
+
+#Add average colum
+students["Average"] = students[["Math", "Python", "Networking"]].mean(axis = 1)
 
 print(students)
+
+print("==============================")
+
+#Find highest and lowest student
+highest_student = students.loc[students["Average"].idxmax()]
+print(f" THE HIGHEST STUDENT IS : \n{highest_student}")
+
+print("==============================")
+
+lowest_student = students.loc[students["Average"].idxmin()]
+print(f" THE LOWEST STUDENT IS : \n{lowest_student}")
+
+print("==============================")
+
+#Count students with an average => 50
+passed_students = students[students["Average"] >= 50]
+print("THE PASSED STUDENTS ARE: ")
+print(passed_students[["Name", "Average"]])
+
+print("==============================")
+
+#create a result column pass >= 50, fail < 50
+students["Result"] = students["Average"].apply(lambda x: "Pass" if x >= 50 else "Fail")
+print(students)   
+
+print("==============================")
+#Highest mark in every subject
+print("HIGHEST MARK IN MATH IS: ", students["Math"].max())
+print("HIGHEST MARK IN PYTHON IS: ", students["Python"].max())
+print("HIGHEST MARK IN NETWORKING IS: ", students["Networking"].max())
