315afc986854bba8c2e35afa45566f8899017b1e
deehdan <waruidedan12@gmail.com>
2026-09-25 00:25:12 +0300

    added range.py

---
+++ range.py
@@ -0,0 +1,8 @@
+number = 5
+for i in range(1, 11):
+    answer = number * i
+    print(f"{number} * {i} = {answer}")
+
+names = ["Ddn", "Cate", "Kamau", "Kimani"]
+for j in range(len(names)):
+    print(f"Student's {j + 1} name is {names[j]}")
